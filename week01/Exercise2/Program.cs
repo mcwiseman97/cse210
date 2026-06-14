216b3dbcb3409558a5f47c7ea37314af90bba2ac
Michael Wiseman <mcwiseman97@gmail.com>
2026-06-14 10:58:38 -0500

    Refactor grade calculation logic to improve clarity and fix sign assignment for grades

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,14 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
+// initial inquiry for percentage and persing to int
         Console.Write("Please provide your current grade percentage: ");
         string gradePercentage = Console.ReadLine();
         int percent = int.Parse(gradePercentage);
 
+// Create variable placeholders
         int endNumber = 0;
         string grade = "";
         string sign = "";
 
+//Finds the grade associated to the percent
         if (percent >= 90)
         {
             grade = "A";
@@ -32,19 +35,26 @@ class Program
             grade = "F" ;
         }
 
-// Add a fix to acount for there being no A+ F+ or F-
+// Added a fix to acount for there being no A+ F+ or F-
         endNumber = percent % 10;
-        if (endNumber >= 7)
+        if (percent > 96 || percent < 60)
         {
-            sign = "+";
-        }
-        else if (endNumber <= 3)
-        {
-            sign = "-";
+            sign = "";
         }
         else
         {
-            sign = "";
+            if (endNumber >= 7)
+            {
+                sign = "+";
+            }
+            else if (endNumber <= 3)
+            {
+                sign = "-";
+            }
+            else
+            {
+                sign = "";
+            }
         }
 
         Console.WriteLine($"Your current grade is a(n) {grade}{sign}");
