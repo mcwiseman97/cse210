134faec8e604deaf0811b6399129579090a5b913
Michael Wiseman <mcwiseman97@gmail.com>
2026-06-14 10:47:00 -0500

    Implement grade calculation based on user input percentage

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Please provide your current grade percentage: ");
+        string gradePercentage = Console.ReadLine();
+        int percent = int.Parse(gradePercentage);
+
+        int endNumber = 0;
+        string grade = "";
+        string sign = "";
+
+        if (percent >= 90)
+        {
+            grade = "A";
+        }
+        else if (percent >= 80){
+            grade = "B";
+        }
+        else if (percent >= 70)
+        {
+            grade = "C";
+        }
+        else if (percent >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F" ;
+        }
+
+        endNumber = percent % 10;
+        if (endNumber >= 7)
+        {
+            sign = "+";
+        }
+        else if (endNumber <= 3)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"Your current grade is a(n) {grade}{sign}");
+
     }
 }
