ffe8c91432194a42a5922d190db113d8e761df83
Michael Wiseman <mcwiseman97@gmail.com>
2026-06-14 21:44:59 -0500

    Remove commented-out documentation and example output from Program.cs

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -40,18 +40,3 @@ class Program
         DisplayResult(usersName, numberSquared);
     }
 }
-
-/*
-DisplayWelcome - Displays the message, "Welcome to the Program!"
-PromptUserName - Asks for and returns the user's name (as a string)
-PromptUserNumber - Asks for and returns the user's favorite number (as an integer)
-SquareNumber - Accepts an integer as a parameter and returns that number squared (as an integer)
-DisplayResult - Accepts the user's name and the squared number and displays them.
-
-
-Welcome to the program!
-Please enter your name: Brother Burton
-Please enter your favorite number: 42
-Brother Burton, the square of your number is 1764
-
-*/
