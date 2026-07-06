fff0591ebd0ecf7e618bf2de800384638e2e1714
Michael Wiseman <mcwiseman97@gmail.com>
2026-07-05 22:29:48 -0500

    Add Job class and implement job title display in Program.cs

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,5 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Team Lead";
+
+        Console.WriteLine($"{job1._jobTitle}");
     }
 }
