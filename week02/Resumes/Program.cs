842a841cf3dc8c185b2481e4e94d09f750a63269
Michael Wiseman <mcwiseman97@gmail.com>
2026-07-06 20:49:07 -0500

    Add Resume class and implement Display method for resume details

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -10,8 +10,13 @@ class Program
         job1._jobTitle = "Team Lead";
         job1._startYear = 2022;
         job1._endYear = 2026;
+        
 
         job1.Display();
+
+        Resume myResume = new Resume();
+        myResume._name = "Michael Wiseman";
+
 
     }
 }
---
+++ week02/Resumes/Resume.cs
@@ -0,0 +1,13 @@
+public class Resume
+{
+    public string _name;
+
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine($"{_name}");
+        Console.WriteLine("Jobs");
+    }
+
+}
