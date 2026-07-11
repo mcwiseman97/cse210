208f1d99e999ff0a83268dec2ed17dcb45f6d9f7
Michael Wiseman <mcwiseman97@gmail.com>
2026-07-11 09:10:34 -0500

    Fix variable naming and assignment in PromptGenerator class
    Fixed missing _ in front of _random

--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -4,12 +4,12 @@ using System.Collections.Generic;
 public class PromptGenerator
 {
     public List<string> _prompts;
-    private Random random;
+    private Random _random;
     private string _lastPrompt;
 
     public PromptGenerator()
     {
-        _random - new Random();
+        _random = new Random();
         _lastPrompt = "";
         _prompts = new List<string>
         {
@@ -34,7 +34,8 @@ public class PromptGenerator
         string prompt;
         do
         {
-            
-        } while ();
+            int index = _random.Next(_prompts.Count);
+            prompt = _prompts[index];
+        } while (prompt == _lastPrompt && _prompts.Count > 1);
     }
 }
