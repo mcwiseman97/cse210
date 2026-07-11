a4000f55b00cf2404f8b5e68afb4ab8ef69e81ec
Michael Wiseman <mcwiseman97@gmail.com>
2026-07-11 09:22:33 -0500

    Fix choice comparison in main loop and add method stubs for journal operations

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Runtime.InteropServices.Marshalling;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
@@ -10,7 +11,7 @@ class Program
 
         string choice = "";
         
-        while (choice != 6)
+        while (choice != "6")
         {
             Console.WriteLine("");
             Console.WriteLine("");
@@ -22,6 +23,54 @@ class Program
             Console.Write("");
             choice = Console.ReadLine();
             Console.WriteLine();
+
+            if (choice == "1")
+            {
+                WriteNewEntry(Journal, promptGenerator);
+            }
+            else if (choice == "2")
+            {
+                
+            }
+            else if (choice == "3")
+            {
+                
+            }
+            else if (choice == "4")
+            {
+                
+            }
+            else if (choice == "6")
+            {
+                
+            }
+            else if (choice == "6")
+            {
+                
+            }
+            else
+            {
+                Console.WriteLine("PLease choose a valid option from the menu.");
+                Console.WriteLine();
+            }
         }
     }
+
+    static void WriteNewEntry(Journal journal, PromptGenerator promptGenerator)
+    {
+        
+    }
+
+    static void SaveJournal(Journal journal)
+    {
+        
+    }
+
+    static void LoadJournal(Journal journal)
+    {
+        
+    }
+
+    static void SearchJournal(Journal journal)
+
 }
