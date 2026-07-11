04c6af68c31865b781d860815dc7bf322e4f37c7
Michael Wiseman <mcwiseman97@gmail.com>
2026-07-11 08:59:51 -0500

    Refactor Entry class to improve constructor and save string methods

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq.Expressions;
 
 public class Entry
 {
@@ -6,4 +7,36 @@ public class Entry
     public string _promptText;
     public string _entryText;
     public string _mood;
+
+    public Entry()
+    {
+        _date = "";
+        _promptText = "";
+        _entryText = "";
+        _mood = "";        
+    }
+
+    public Entry(string date, string promptText, string entryText, string mood)
+    {
+        Console.WriteLine($"Date: {_date} - Prompt: {_promptText}");
+        Console.WriteLine($"Mood: {_mood}");
+        Console.WriteLine(_entryText);
+        Console.WriteLine();
+    }
+
+    public string GetSaveString()
+    {
+        return $"{_date}|{_promptText}|{_mood}|{_entryText}"
+    }
+    public static Entry CreateFromSaveString(string line)
+    {
+        string[] parts = line.Split("|");
+
+        if (parts.Length >= 4)
+        {
+            return new Entry(parts[0], parts[1], parts[3], parts[2]);
+        }
+
+        return new Entry();
+    }
 }
