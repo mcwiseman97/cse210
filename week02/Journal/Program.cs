f8033cfb88c04a2a71768cbcc89295b7e6da8117
Michael Wiseman <mcwiseman97@gmail.com>
2026-07-11 09:16:27 -0500

    Add main method implementation with journal and prompt generator initialization

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Runtime.InteropServices.Marshalling;
 
 class Program
 {
     static void Main(string[] args)
     {
+        Journal journal = new Journal();
+        PromptGenerator promptGenerator = new PromptGenerator();
+
+        string choice = "";
         
+        while (choice != 6)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.Write("");
+            choice = Console.ReadLine();
+            Console.WriteLine();
+        }
     }
 }
