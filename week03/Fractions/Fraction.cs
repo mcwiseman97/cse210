b78bcce5b0c1df5db6577c2785c28cbca5416326
Michael Wiseman <mcwiseman97@gmail.com>
2026-07-15 12:52:20 -0500

    Add constructors to Fraction class for whole numbers and numerator/denominator pairs; update Program.cs to demonstrate new functionality.

--- week03/Fractions/Fraction.cs
+++ week03/Fractions/Fraction.cs
@@ -9,11 +9,22 @@ public class Fraction
         _bottom = 1;
     }
 
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
     public string GetFractionString()
     {
         string text = $"{_top}/{_bottom}";
         return text;
-
     }
 
     public double GetDecimalValue()
