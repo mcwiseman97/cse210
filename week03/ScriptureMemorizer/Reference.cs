af59e408a6ab976776c0d1f503006f12f2381069
Michael Wiseman <mcwiseman97@gmail.com>
2026-07-15 13:44:33 -0500

    Add initial implementations of Reference, Scripture, and Word classes with constructors and basic methods for text display and visibility management.

---
+++ week03/ScriptureMemorizer/Reference.cs
@@ -0,0 +1,23 @@
+public class Reference
+{
+    private string _book;
+    private int _chapter;
+    private int _verse;
+    private int _startVerse;
+    private int _endVerse;
+
+    public Reference(string _book, int _chapter, int _verse)
+    {
+
+    }
+
+    public Reference(string _book, int _chapter, int _startVerse, int _endVerse)
+    {
+        
+    }
+
+    public void GetDisplayText()
+    {
+        
+    }
+}
---
+++ week03/ScriptureMemorizer/Word.cs
@@ -0,0 +1,32 @@
+using System.Net;
+
+public class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string _text)
+    {
+
+    }
+
+    public void Hide()
+    {
+        
+    }
+
+    public void Show()
+    {
+
+    }
+
+    public bool IsHidden()
+    {
+        return _isHidden;
+    }
+
+    public string GetDisplayText()
+    {
+        return _text; 
+    }
+}
