130d3c623f5b9d48db1ff80c0a25806438ce6dcb
Michael Wiseman <mcwiseman97@gmail.com>
2026-07-15 13:26:47 -0500

    Remove initial greeting from Program.cs to streamline output for the Fractions project.

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,7 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
 
         Fraction f1 = new Fraction();
         Console.WriteLine(f1.GetFractionString());
