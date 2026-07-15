b64bc54c5685d4ac076a20ce41f5cffdb002e88c
Michael Wiseman <mcwiseman97@gmail.com>
2026-07-15 14:17:50 -0500

    Add functionality to Scripture and Word classes for word hiding and displaying; initialize word list in Scripture constructor and update visibility methods in Word class.

--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -3,11 +3,20 @@ public class Scripture
 
     private Reference _reference;
     private List<Word> _words;
+    private Random _random;
 
 
     public Scripture(Reference reference, string text)
     {
+        _reference = reference;
+        _words = new List<Word>();
+        _random = new Random();
+        string[] parts = text.Split(" ", StringSplitOptions.RemoveEmptyEntries);
 
+        foreach (string part in parts)
+        {
+            _words.Add(new Word(part));
+        }
     }
 
     public void HideRandomWords(int numberToHide)
--- week03/ScriptureMemorizer/Word.cs
+++ week03/ScriptureMemorizer/Word.cs
@@ -11,12 +11,12 @@ public class Word
 
     public void Hide()
     {
-        
+        _isHidden = true;
     }
 
     public void Show()
     {
-
+        _isHidden = false;
     }
 
     public bool IsHidden()
@@ -26,6 +26,10 @@ public class Word
 
     public string GetDisplayText()
     {
+        if (_isHidden)
+        {
+            return new string('_', _text.Length);
+        }
         return _text; 
     }
 }
