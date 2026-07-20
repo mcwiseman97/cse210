6bc663bce7b17c4d82f5d1c844026dbed7e81034
Michael Wiseman <mcwiseman97@gmail.com>
2026-07-19 22:34:44 -0500

    Implement Comment and Video classes with methods for adding comments and retrieving comment count

--- week04/YouTubeVideos/Comment.cs
+++ week04/YouTubeVideos/Comment.cs
@@ -1,3 +1,15 @@
 // _commenterName : string
 // _commentText : string
 // Comment(commenterName : string, commentText : string)
+
+class Comment
+{
+    private string _commenterName;
+    private string _commentText;
+
+    public Comment(string commenterName, string commentText)
+    {
+        _commenterName = commenterName;
+        _commentText = commentText;
+    }
+}
--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+
+        // Todo: create 3 videos and add at least 3 comments to each video. Then display the details of each video along with its comments. 
+        
+
+
     }
 }
