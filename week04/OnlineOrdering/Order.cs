da23231afdc5df9e39d00de68e7966b0420da04d
Michael Wiseman <mcwiseman97@gmail.com>
2026-07-21 09:46:17 -0500

    Refactor Order class constructor to accept PackingLabel and ShippingLabel lists

--- week04/OnlineOrdering/Order.cs
+++ week04/OnlineOrdering/Order.cs
@@ -3,17 +3,17 @@ class Order {
     // Variable declaration
     private double _totalPrice;
     private string _countryOrigin;
-    private List<string> _packingLabel;
-    private List<string> _shippingLabel;
+    private List<PackingLabel> _packingLabel;
+    private List<ShippingLabel> _shippingLabel;
 
 
-    Public Order()
+    Public Order(double totalPrice, string countryOrigin, List<PackingLabel> packingLabel, List<ShippingLabel> shippingLabel) 
     {
-        _totalPrice = 0.0;
-        _countryOrigin = "";
-        _packingLabel = new List<string>();
-        _shippingLabel = new List<string>();
-        
+        _totalPrice = totalPrice;
+        _countryOrigin = countryOrigin;
+        _packingLabel = packingLabel;
+        _shippingLabel = shippingLabel;
     }
+
     
 }
