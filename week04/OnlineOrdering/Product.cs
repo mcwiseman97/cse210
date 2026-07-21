f8ebda848584485818d138629054e7ec8ec35118
Michael Wiseman <mcwiseman97@gmail.com>
2026-07-21 09:48:48 -0500

    Implement Product class with properties for name, product ID, and quantity

---
+++ week04/OnlineOrdering/Product.cs
@@ -0,0 +1,12 @@
+class Product
+{
+    private string _name;
+    private string _productId;
+    private double _quantity;
+    public Product(string name, string productId, double quantity)
+    {
+        _name = name;
+        _productId = productId;
+        _quantity = quantity;
+    }
+}
