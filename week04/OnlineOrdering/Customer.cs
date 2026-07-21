816c0fdc0a50109319b068279eface48dd646e2d
Michael Wiseman <mcwiseman97@gmail.com>
2026-07-21 09:50:38 -0500

    Implement Customer class with properties for name, address, and return option

---
+++ week04/OnlineOrdering/Customer.cs
@@ -0,0 +1,14 @@
+class Customer
+{
+    private string _name;
+    private string _address;
+    private string _returnOption;
+
+    public Customer(string name, string address, string returnOption)
+    {
+        _name = name;
+        _address = address;
+        _returnOption = returnOption;
+    }
+
+}
