b8226b55d62bdb083d69772fac052f6ce97e968e
Michael Wiseman <mcwiseman97@gmail.com>
2026-07-26 15:21:57 -0500

    Refactor Main method in Program class to improve readability by organizing assignment instances

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+
+        WritingAssignment writingAssignment1 = new WritingAssignment();
+        WritingAssignment writingAssignment2 = new WritingAssignment();
+        WritingAssignment writingAssignment3 = new WritingAssignment();
+
+
+
+
+        MathAssignment mathAssignment1 = new MathAssignment();
+        MathAssignment mathAssignment2 = new MathAssignment();
+        MathAssignment mathAssignment3 = new MathAssignment();
+
+
     }
 }
