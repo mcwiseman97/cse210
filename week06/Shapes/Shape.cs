ccbaa25b92af8228236c92759fdaa7330f55eaad
Michael Wiseman <mcwiseman97@gmail.com>
2026-08-02 20:20:46 -0500

    Add initial structure for Circle, Rectangle, Square classes and implement Shape class with color management methods

---
+++ week06/Shapes/Shape.cs
@@ -0,0 +1,16 @@
+public abstract class Shape
+{
+    private string _color;
+    public Shape()
+    {
+        public string GetColor
+        {
+
+        }
+        public void SetColor(string color)
+        {
+            _color = color;
+        }
+        
+    }
+}
