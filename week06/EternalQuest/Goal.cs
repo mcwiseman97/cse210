cabdf4b03ec6b7e119350a0ca9c472e8544a8da5
Michael Wiseman <mcwiseman97@gmail.com>
2026-08-04 21:20:17 -0500

    Implement Goal, GoalManager, and SimpleGoal classes with constructors and method overrides

--- week06/EternalQuest/Goal.cs
+++ week06/EternalQuest/Goal.cs
@@ -3,4 +3,31 @@ class Goal
     private string _shortName;
     private string _description;
     private int _points;
+
+    public Goal(string shortName, string description, int points)
+    {
+        _shortName = shortName;
+        _description = description;
+        _points = points;
+    }
+
+    public void RecordEvent()
+    {
+        
+    }
+
+    public bool IsComplete()
+    {
+        return false;
+    }
+
+    public string GetDetailsString()
+    {
+        return $"{_shortName}: {_description} ({_points} points)";
+    }
+
+    public string GetStringRepresntation()
+    {
+        return $"{_shortName}|{_description}|{_points}";
+    }
 }
--- week06/EternalQuest/GoalManager.cs
+++ week06/EternalQuest/GoalManager.cs
@@ -2,4 +2,49 @@ class GoalManager
 {
     private List<Goal> _goals;
     private int _score;
+
+    public class GoalManager()
+    {
+        _goals = new List<Goal>();
+        _score = 0;
+    }
+    public void Start()
+    {
+        // Load goals from file
+        // Display menu to user
+        // Handle user input
+    }
+    public void DisplayPlayerInfo()
+    {
+        // Display player's info
+    }
+    public void ListGoalNames()
+    {
+        // List the names of all goals
+    }
+
+    public void ListGoalDetails()
+    {
+        // List the details of all goals
+    }
+
+    public void CreateGoal()
+    {
+        // Create a new goal
+    }
+
+    public void RecordEvent()
+    {
+        // Record an event for a goal
+    }
+
+    public void SaveGoals()
+    {
+        // Save goals to file
+    }
+
+    public void LoadGoals()
+    {
+        // Load goals from file
+    }
 }
