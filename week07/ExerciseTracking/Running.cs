d3bb87e669f7efaa152b8bb929fe83dd3540efb6
Michael Wiseman <mcwiseman97@gmail.com>
2026-08-07 21:47:50 -0500

    Add Running class with distance tracking and implement methods for speed and pace calculations

--- week07/ExerciseTracking/Running.cs
+++ week07/ExerciseTracking/Running.cs
@@ -1,6 +1,26 @@
+using System;
+
 class Running : Activity
 {
-    public Running() : base()
+    private double _distance;
+
+    public Running(DateTime date, int minutes, double distance) : base(date, minutes)
     {
+        _distance = distance;
+    }
+
+    public override double GetDistance()
+    {
+        return _distance;
+    }
+
+    public override double GetSpeed()
+    {
+        return (GetDistance() / GetMinutes()) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return GetMinutes() / GetDistance();
     }
 }
