6248dd8c9f7f6a02a0e89f83e6eadd56829d3ab7
Michael Wiseman <mcwiseman97@gmail.com>
2026-08-07 21:43:29 -0500

    Add Swimming class with lap tracking and distance, speed, and pace calculations

--- week07/ExerciseTracking/Swimming.cs
+++ week07/ExerciseTracking/Swimming.cs
@@ -1,7 +1,26 @@
+using System;
+
 class Swimming : Activity
 {
-    public Swimming() : base()
-    {
+    private int _laps;
 
+    public Swimming(DateTime date, int minutes, int laps) : base(date, minutes)
+    {
+        _laps = laps;
+    }
+
+    public override double GetDistance()
+    {
+        return _laps * 50 / 1000.0 * 0.62;
+    }
+
+    public override double GetSpeed()
+    {
+        return (GetDistance() / GetMinutes()) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return GetMinutes() / GetDistance();
     }
 }
