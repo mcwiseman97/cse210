ec1fbcdc9add1e98a631ef91ff3d3f7298d28f50
Michael Wiseman <mcwiseman97@gmail.com>
2026-08-07 21:54:40 -0500

    Add Cycling class with speed tracking and implement methods for distance and pace calculations

--- week07/ExerciseTracking/Cycling.cs
+++ week07/ExerciseTracking/Cycling.cs
@@ -1,6 +1,26 @@
+using System;
+
 class Cycling : Activity
 {
-    public Cycling() : base()
+    private double _speed;
+
+    public Cycling(DateTime date, int minutes, double speed) : base(date, minutes)
     {
+        _speed = speed;
+    }
+
+    public override double GetDistance()
+    {
+        return GetSpeed() * GetMinutes() / 60;
+    }
+
+    public override double GetSpeed()
+    {
+        return _speed;
+    }
+
+    public override double GetPace()
+    {
+        return 60 / GetSpeed();
     }
 }
